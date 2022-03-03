8e436a071425f8cf3646f599ccdb9dbe58c4dea3
JasonDeKhanh <giakhanh.lehoang@gmail.com>
2022-03-04 02:16:42 +0800

    Added Decoration entity and GiftCard entity; fixed unitPrice getter setter Flower

--- BouquetStorySystem-ejb/src/java/entity/Decoration.java
+++ BouquetStorySystem-ejb/src/java/entity/Decoration.java
@@ -7,7 +7,6 @@ package entity;
 
 import java.io.Serializable;
 import java.math.BigDecimal;
-import java.util.ArrayList;
 import java.util.List;
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -29,12 +28,12 @@ import util.enumeration.Occasion;
  * @author msipc
  */
 @Entity
-public class Flower implements Serializable {
+public class Decoration implements Serializable {
 
     private static final long serialVersionUID = 1L;
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long flowerId;
+    private Long decorationId;
     @Column(nullable = false, length = 64)
     @NotNull
     @Size(max = 64)
@@ -42,11 +41,6 @@ public class Flower implements Serializable {
     @Column(nullable = false)
     @NotNull
     private String imgAddress;
-    @Enumerated(EnumType.STRING)
-    @Column(nullable = false)
-    @NotNull
-    private FlowerColorEnum flowerColor;
-    private List<Occasion> occasions; // bean validation?
     @Column(length = 128)
     @Size(max = 128)
     private String description;
@@ -67,35 +61,27 @@ public class Flower implements Serializable {
     @NotNull
     private Boolean isOnDisplay;
 
-    
-    
-    public Flower() {
-        this.occasions = new ArrayList<Occasion>();
+    public Decoration() {
     }
 
-    public Flower(String name, String imgAddress, FlowerColorEnum flowerColor, List<Occasion> occasions, String description, Integer quantityOnHand, Integer reorderQuantity, Boolean isOnDisplay) {
-        
-        this();
-        
+    public Decoration(String name, String imgAddress, String description, Integer quantityOnHand, Integer reorderQuantity, BigDecimal unitPrice, Boolean isOnDisplay) {
         this.name = name;
         this.imgAddress = imgAddress;
-        this.flowerColor = flowerColor;
-        this.occasions = occasions;
         this.description = description;
         this.quantityOnHand = quantityOnHand;
         this.reorderQuantity = reorderQuantity;
+        this.unitPrice = unitPrice;
         this.isOnDisplay = isOnDisplay;
     }
-    
 
     
     
-    public Long getFlowerId() {
-        return flowerId;
+    public Long getDecorationId() {
+        return decorationId;
     }
 
-    public void setFlowerId(Long flowerId) {
-        this.flowerId = flowerId;
+    public void setDecorationId(Long decorationId) {
+        this.decorationId = decorationId;
     }
 
     public String getName() {
@@ -114,22 +100,6 @@ public class Flower implements Serializable {
         this.imgAddress = imgAddress;
     }
 
-    public FlowerColorEnum getFlowerColor() {
-        return flowerColor;
-    }
-
-    public void setFlowerColor(FlowerColorEnum flowerColor) {
-        this.flowerColor = flowerColor;
-    }
-
-    public List<Occasion> getOccasions() {
-        return occasions;
-    }
-
-    public void setOccasions(List<Occasion> occasions) {
-        this.occasions = occasions;
-    }
-
     public String getDescription() {
         return description;
     }
@@ -146,20 +116,20 @@ public class Flower implements Serializable {
         this.quantityOnHand = quantityOnHand;
     }
 
-    public BigDecimal getUnitPrice() {
-        return unitPrice;
-    }
-
-    public void setUnitPrice(BigDecimal unitPrice) {
-        this.unitPrice = unitPrice;
-    }
-
     public Integer getReorderQuantity() {
         return reorderQuantity;
     }
 
     public void setReorderQuantity(Integer reorderQuantity) {
         this.reorderQuantity = reorderQuantity;
+    }
+
+    public BigDecimal getUnitPrice() {
+        return unitPrice;
+    }
+
+    public void setUnitPrice(BigDecimal unitPrice) {
+        this.unitPrice = unitPrice;
     }
 
     public Boolean getIsOnDisplay() {
@@ -173,18 +143,18 @@ public class Flower implements Serializable {
     @Override
     public int hashCode() {
         int hash = 0;
-        hash += (flowerId != null ? flowerId.hashCode() : 0);
+        hash += (decorationId != null ? decorationId.hashCode() : 0);
         return hash;
     }
 
     @Override
     public boolean equals(Object object) {
         // TODO: Warning - this method won't work in the case the id fields are not set
-        if (!(object instanceof Flower)) {
+        if (!(object instanceof Decoration)) {
             return false;
         }
-        Flower other = (Flower) object;
-        if ((this.flowerId == null && other.flowerId != null) || (this.flowerId != null && !this.flowerId.equals(other.flowerId))) {
+        Decoration other = (Decoration) object;
+        if ((this.decorationId == null && other.decorationId != null) || (this.decorationId != null && !this.decorationId.equals(other.decorationId))) {
             return false;
         }
         return true;
@@ -192,7 +162,7 @@ public class Flower implements Serializable {
 
     @Override
     public String toString() {
-        return "entity.Flower[ id=" + flowerId + " ]";
+        return "entity.Decoration[ id=" + decorationId + " ]";
     }
     
 }
