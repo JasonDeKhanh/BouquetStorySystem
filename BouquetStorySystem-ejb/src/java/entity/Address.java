6acbfd5f0b9835f5af307331561809637c31dc34
JasonDeKhanh <giakhanh.lehoang@gmail.com>
2022-03-26 00:41:05 +0800

    Added Address entity

---
+++ BouquetStorySystem-ejb/src/java/entity/Address.java
@@ -0,0 +1,98 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entity;
+
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+/**
+ *
+ * @author msipc
+ */
+@Entity
+public class Address implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long addressId;
+    @Column(nullable = false, length = 255)
+    @NotNull
+    @Size(max = 255)
+    private String line;
+    @Column(nullable = false, length = 10)
+    @NotNull
+    @Size(max = 10)
+    private String postCode;
+
+    public Address() {
+    }
+
+    public Address(String line, String postCode) {
+        this();
+        
+        this.line = line;
+        this.postCode = postCode;
+    }
+    
+    
+
+    public Long getAddressId() {
+        return addressId;
+    }
+
+    public void setAddressId(Long addressId) {
+        this.addressId = addressId;
+    }
+
+    public String getLine() {
+        return line;
+    }
+
+    public void setLine(String line) {
+        this.line = line;
+    }
+
+    public String getPostCode() {
+        return postCode;
+    }
+
+    public void setPostCode(String postCode) {
+        this.postCode = postCode;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (addressId != null ? addressId.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Address)) {
+            return false;
+        }
+        Address other = (Address) object;
+        if ((this.addressId == null && other.addressId != null) || (this.addressId != null && !this.addressId.equals(other.addressId))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "entity.Address[ id=" + addressId + " ]";
+    }
+    
+}
