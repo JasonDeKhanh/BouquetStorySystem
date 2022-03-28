f4778c0583657609d03e861d43468822ab352fae
JasonDeKhanh <giakhanh.lehoang@gmail.com>
2022-03-29 00:11:52 +0800

    Started implementing image uploading for Create New Decoration. WIP.

--- BouquetStorySystem-war/src/java/jsf/managedBean/DecorationManagementManagedBean.java
+++ BouquetStorySystem-war/src/java/jsf/managedBean/DecorationManagementManagedBean.java
@@ -7,18 +7,20 @@ package jsf.managedBean;
 
 import ejb.stateless.DecorationSessionBeanLocal;
 import entity.Decoration;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.Serializable;
-import java.util.ArrayList;
 import java.util.List;
 import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
 import javax.inject.Named;
-import javax.enterprise.context.RequestScoped;
 import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
 import javax.faces.event.ActionEvent;
 import javax.faces.view.ViewScoped;
+import org.primefaces.event.FileUploadEvent;
 import util.exception.CreateNewDecorationException;
 import util.exception.DecorationNotFoundException;
 import util.exception.DeleteDecorationException;
@@ -43,6 +45,9 @@ public class DecorationManagementManagedBean implements Serializable {
     private Decoration newDecorationEntity;
     
     private Decoration selectedDecorationEntityToUpdate;
+    
+    private String uploadedFilePath;
+    private Boolean showUploadedFile;
     
     public DecorationManagementManagedBean() {
         newDecorationEntity = new Decoration();
@@ -127,6 +132,60 @@ public class DecorationManagementManagedBean implements Serializable {
         }
     }
 
+    
+    public void handleImageUpload(FileUploadEvent event)
+    {
+        // from Prof's Lecture 6 Demo 3
+        try
+        {
+//            String newFilePath = FacesContext.getCurrentInstance().getExternalContext().getInitParameter("alternatedocroot_2") + System.getProperty("file.separator") + event.getFile().getFileName();
+            String test = FacesContext.getCurrentInstance().getExternalContext().getInitParameter("alternatedocroot_2");
+            String test2 = System.getProperty("file.separator");
+            String test3 = event.getFile().getFileName();
+            System.out.println("test: " + test);
+            System.out.println("test2: " + test2);
+            System.out.println("test3: " + test3);
+            String newFilePath = test + test2 + test3;
+            
+            System.err.println("********** Bouquet Story System: File name: " + event.getFile().getFileName());
+            System.err.println("********** Bouquet Story System: newFilePath: " + newFilePath);
+
+            File file = new File(newFilePath);
+            FileOutputStream fileOutputStream = new FileOutputStream(file);
+
+            int a;
+            int BUFFER_SIZE = 8192;
+            byte[] buffer = new byte[BUFFER_SIZE];
+
+            InputStream inputStream = event.getFile().getInputStream();
+
+            while (true)
+            {
+                a = inputStream.read(buffer);
+
+                if (a < 0)
+                {
+                    break;
+                }
+
+                fileOutputStream.write(buffer, 0, a);
+                fileOutputStream.flush();
+            }
+
+            fileOutputStream.close();
+            inputStream.close();
+            
+            setUploadedFilePath(FacesContext.getCurrentInstance().getExternalContext().getInitParameter("uploadedFilesPath") + "/" + event.getFile().getFileName());
+            setShowUploadedFile((Boolean) true);
+            
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO,  "File uploaded successfully", ""));
+        }
+        catch(IOException ex)
+        {
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR,  "File upload error: " + ex.getMessage(), ""));
+        }
+    }
+    
     /**
      * @return the decorationEntities
      */
@@ -181,6 +240,22 @@ public class DecorationManagementManagedBean implements Serializable {
      */
     public void setSelectedDecorationEntityToUpdate(Decoration selectedDecorationEntityToUpdate) {
         this.selectedDecorationEntityToUpdate = selectedDecorationEntityToUpdate;
+    }
+
+    public String getUploadedFilePath() {
+        return uploadedFilePath;
+    }
+
+    public void setUploadedFilePath(String uploadedFilePath) {
+        this.uploadedFilePath = uploadedFilePath;
+    }
+
+    public Boolean getShowUploadedFile() {
+        return showUploadedFile;
+    }
+
+    public void setShowUploadedFile(Boolean showUploadedFile) {
+        this.showUploadedFile = showUploadedFile;
     }
 
     
