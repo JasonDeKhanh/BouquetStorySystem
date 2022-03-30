beace69309dcea99d746142ad7c1de7da92775c8
JasonDeKhanh <giakhanh.lehoang@gmail.com>
2022-03-30 15:15:11 +0800

    WIP containertype stuff

--- BouquetStorySystem-war/src/java/jsf/managedBean/ContainerTypeManagementManagedBean.java
+++ BouquetStorySystem-war/src/java/jsf/managedBean/ContainerTypeManagementManagedBean.java
@@ -5,9 +5,14 @@
  */
 package jsf.managedBean;
 
+import ejb.stateless.ContainerTypeSessionBeanLocal;
+import entity.ContainerType;
 import javax.inject.Named;
 import javax.faces.view.ViewScoped;
 import java.io.Serializable;
+import java.util.List;
+import javax.ejb.EJB;
+import javax.faces.event.ActionEvent;
 
 /**
  *
@@ -17,10 +22,77 @@ import java.io.Serializable;
 @ViewScoped
 public class ContainerTypeManagementManagedBean implements Serializable {
 
-    /**
-     * Creates a new instance of ContainerTypeManagementManagedBean
-     */
+    @EJB(name = "ContainerTypeSessionBeanLocal")
+    private ContainerTypeSessionBeanLocal containerTypeSessionBeanLocal;
+
+    private List<ContainerType> containerTypes;
+    private List<ContainerType> filteredContainerTypes;
+    
+    private ContainerType newContainerTypeEntity;
+    
+    private ContainerType selectedContainerTypeEntityToUpdate;
+    
+    private String uploadedFilePath;
+    private Boolean showUploadedFile;
+    
+
     public ContainerTypeManagementManagedBean() {
+        newContainerTypeEntity = new ContainerType();
+    }
+
+    
+    public void doUpdateContainerType(ActionEvent event)
+    {
+        selectedContainerTypeEntityToUpdate = (ContainerType)event.getComponent().getAttributes().get("containerTypeToUpdate");
+    }
+    
+    
+    public List<ContainerType> getContainerTypes() {
+        return containerTypes;
+    }
+
+    public void setContainerTypes(List<ContainerType> containerTypes) {
+        this.containerTypes = containerTypes;
+    }
+
+    public List<ContainerType> getFilteredContainerTypes() {
+        return filteredContainerTypes;
+    }
+
+    public void setFilteredContainerTypes(List<ContainerType> filteredContainerTypes) {
+        this.filteredContainerTypes = filteredContainerTypes;
+    }
+
+    public ContainerType getNewContainerTypeEntity() {
+        return newContainerTypeEntity;
+    }
+
+    public void setNewContainerTypeEntity(ContainerType newContainerTypeEntity) {
+        this.newContainerTypeEntity = newContainerTypeEntity;
+    }
+
+    public ContainerType getSelectedContainerTypeEntityToUpdate() {
+        return selectedContainerTypeEntityToUpdate;
+    }
+
+    public void setSelectedContainerTypeEntityToUpdate(ContainerType selectedContainerTypeEntityToUpdate) {
+        this.selectedContainerTypeEntityToUpdate = selectedContainerTypeEntityToUpdate;
+    }
+
+    public String getUploadedFilePath() {
+        return uploadedFilePath;
+    }
+
+    public void setUploadedFilePath(String uploadedFilePath) {
+        this.uploadedFilePath = uploadedFilePath;
+    }
+
+    public Boolean getShowUploadedFile() {
+        return showUploadedFile;
+    }
+
+    public void setShowUploadedFile(Boolean showUploadedFile) {
+        this.showUploadedFile = showUploadedFile;
     }
     
 }
