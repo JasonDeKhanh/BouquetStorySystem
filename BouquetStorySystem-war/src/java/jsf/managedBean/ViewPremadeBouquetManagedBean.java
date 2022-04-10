36950a2f7f2c72a9095c7b008f0fe1540b4a0039
JasonDeKhanh <giakhanh.lehoang@gmail.com>
2022-04-10 20:27:32 +0800

    completed View Premade Bouquet

--- BouquetStorySystem-war/src/java/jsf/managedBean/ViewPremadeBouquetManagedBean.java
+++ BouquetStorySystem-war/src/java/jsf/managedBean/ViewPremadeBouquetManagedBean.java
@@ -5,10 +5,22 @@
  */
 package jsf.managedBean;
 
+import ejb.stateless.DecorationSessionBeanLocal;
+import ejb.stateless.FlowerSessionBeanLocal;
+import ejb.stateless.PremadeBouquetSessionBeanLocal;
+import entity.Decoration;
+import entity.Flower;
 import entity.PremadeBouquet;
 import javax.inject.Named;
 import javax.faces.view.ViewScoped;
 import java.io.Serializable;
+import javax.annotation.PostConstruct;
+import javax.ejb.EJB;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import util.exception.DecorationNotFoundException;
+import util.exception.FlowerNotFoundException;
+import util.exception.PremadeBouquetNotFoundException;
 
 /**
  *
@@ -18,18 +30,101 @@ import java.io.Serializable;
 @ViewScoped
 public class ViewPremadeBouquetManagedBean implements Serializable {
 
-    private PremadeBouquet premadeBouquetEntityToView;
+    @EJB(name = "FlowerSessionBeanLocal")
+    private FlowerSessionBeanLocal flowerSessionBeanLocal;
+
+    @EJB(name = "DecorationSessionBeanLocal")
+    private DecorationSessionBeanLocal decorationSessionBeanLocal;
+
+    @EJB(name = "PremadeBouquetSessionBeanLocal")
+    private PremadeBouquetSessionBeanLocal premadeBouquetSessionBeanLocal;
+
+    // used to help View Details Decoration
+    private Decoration curDecoration;
+    // used to help View Details Flower
+    private Flower curFlower;
+    
+    private Long premadeBouquetId;
+    private PremadeBouquet premadeBouquet;
     
     public ViewPremadeBouquetManagedBean() {
-        premadeBouquetEntityToView = new PremadeBouquet();
+        premadeBouquet = new PremadeBouquet();
     }
 
-    public PremadeBouquet getPremadeBouquetEntityToView() {
-        return premadeBouquetEntityToView;
+    @PostConstruct
+    public void postConstruct()
+    {
+        try 
+        {
+            System.out.println("Inside try block post construct");
+            premadeBouquetId = Long.valueOf(
+                    FacesContext.getCurrentInstance().getExternalContext().getRequestParameterMap().get("premadeBouquetId"));
+            premadeBouquet = premadeBouquetSessionBeanLocal.retrievePremadeBouquetByItemId(premadeBouquetId);
+        } 
+        catch (PremadeBouquetNotFoundException ex) 
+        {
+            System.out.println("inside catch error block postconstruct");
+            FacesContext.getCurrentInstance().addMessage(null,
+                    new FacesMessage(FacesMessage.SEVERITY_WARN, "Premade Bouquet ID not provided", null));
+        }
+    }
+    
+    public Decoration getUpdatedDecoration()
+    {
+        try 
+        {
+            return decorationSessionBeanLocal.retrieveDecorationByDecorationId(curDecoration.getDecorationId());
+        } 
+        catch (DecorationNotFoundException ex) 
+        {
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_WARN, "Decoration not found", null));
+            return null;
+        }
+    }
+    
+    public Flower getUpdatedFlower()
+    {
+        try 
+        {
+            return flowerSessionBeanLocal.retrieveFlowerByFlowerId(curFlower.getFlowerId());
+        } 
+        catch (FlowerNotFoundException ex) 
+        {
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_WARN, "Flower not found", null));
+            return null;
+        }
+    }
+    
+    public PremadeBouquet getPremadeBouquet() {
+        return premadeBouquet;
     }
 
-    public void setPremadeBouquetEntityToView(PremadeBouquet premadeBouquetEntityToView) {
-        this.premadeBouquetEntityToView = premadeBouquetEntityToView;
+    public void setPremadeBouquet(PremadeBouquet premadeBouquet) {
+        this.premadeBouquet = premadeBouquet;
+    }
+
+    public Long getPremadeBouquetId() {
+        return premadeBouquetId;
+    }
+
+    public void setPremadeBouquetId(Long premadeBouquetId) {
+        this.premadeBouquetId = premadeBouquetId;
+    }
+
+    public Decoration getCurDecoration() {
+        return curDecoration;
+    }
+
+    public void setCurDecoration(Decoration curDecoration) {
+        this.curDecoration = curDecoration;
+    }
+
+    public Flower getCurFlower() {
+        return curFlower;
+    }
+
+    public void setCurFlower(Flower curFlower) {
+        this.curFlower = curFlower;
     }
     
 }
