a9f78919f493100524bac73ab858cae3fd7a2a45
MatthiasTanWeiSheng <e0540243@u.nus.edu>
2022-04-16 02:09:22 +0800

    Jasper Report Added

---
+++ BouquetStorySystem-war/src/java/jsf/managedBean/adminReportManagedBean.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jsf.managedBean;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.sql.SQLException;
+import java.util.HashMap;
+import javax.annotation.Resource;
+import javax.inject.Named;
+import javax.enterprise.context.RequestScoped;
+import javax.faces.context.FacesContext;
+import javax.faces.event.ActionEvent;
+import javax.sql.DataSource;
+import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JasperRunManager;
+
+/**
+ *
+ * @author matt_
+ */
+@Named(value = "adminReportManagedBean")
+@RequestScoped
+public class adminReportManagedBean {
+
+    @Resource(name = "bouquetStorySystemDataSource")
+    private DataSource bouquetStorySystemDataSource;
+    
+    
+
+    /**
+     * Creates a new instance of adminReportManagedBean
+     */
+    public adminReportManagedBean() {
+    }
+    
+    public void generateReport(ActionEvent event) {
+        
+        try {
+            HashMap parameters = new HashMap();
+           
+            
+
+            InputStream reportStream = FacesContext.getCurrentInstance().getExternalContext().getResourceAsStream("/jasperreport/adminreport.jasper");
+            OutputStream outputStream = FacesContext.getCurrentInstance().getExternalContext().getResponseOutputStream();
+         
+            JasperRunManager.runReportToPdfStream(reportStream, outputStream, parameters, bouquetStorySystemDataSource.getConnection());
+            
+        } catch (JRException ex) {
+            ex.printStackTrace();
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        } catch (IOException ex) {
+        }
+    }
+    
+}
